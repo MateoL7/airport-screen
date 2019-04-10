ad9709a2c026e30a649a15948c8bdc15a429671d
Mateo Loaiza <thetraceurs@gmail.com>
2019-04-10 17:25:07 -0500

    I added some tests and their corresponding design

--- test/model/AirportTest.java
+++ test/model/AirportTest.java
@@ -8,23 +8,97 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 class AirportTest {
-	
+
 	private Airport ap;
 
 	public void setupScenary1() {
-		
+
 	}
-	
+
 	public void setupScenary2() throws IOException {
 		ap = new Airport(20);
 	}
-	
+
+	public void setupScenary3() throws IOException {
+		ap.randomFlightList(10);
+	}
+
 	@Test
 	public void testTime() throws IOException {
 		setupScenary2();
 		double x = 100.0;
 		String msg = ap.time(x);
 		assertTrue("Not the right value", msg.equalsIgnoreCase("am"));
+	}
+
+	@Test
+	public void testSortByGate() throws IOException {
+		setupScenary2();
+		setupScenary3();
+		ap.sortByGate();
+		for(int i = 0; i < ap.getFlights().length-1; i++) {
+			assertTrue("Not the right place", ap.getFlights()[i].getGate() <= ap.getFlights()[i+1].getGate());
+		}
+	}
+	@Test
+	public void testSearchFlightTime() throws IOException {
+		setupScenary2();
+		setupScenary3();
+		String time = "AM 7:00";
+		Flight x = ap.searchFlightTime(time);
+		if(x != null) {
+		assertTrue("Not working", x.getTime().equalsIgnoreCase(time));
+		}
+	}
+	@Test
+	public void testSearchFlightDate() throws IOException {
+		setupScenary2();
+		setupScenary3();
+		String date = "2010/11/11";
+		Flight x = ap.searchFlightTime(date);
+		if(x != null) {
+		assertTrue("Not working", x.getTime().equalsIgnoreCase(date));
+		}
+	}
+	@Test
+	public void testSearchFlightAirline() throws IOException {
+		setupScenary2();
+		setupScenary3();
+		String search = "Avianca";
+		Flight x = ap.searchFlightTime(search);
+		if(x != null) {
+		assertTrue("Not working", x.getTime().equalsIgnoreCase(search));
+		}
+	}
+	@Test
+	public void testSearchFlightId() throws IOException {
+		setupScenary2();
+		setupScenary3();
+		String search = "ABC123";
+		Flight x = ap.searchFlightTime(search);
+		if(x != null) {
+		assertTrue("Not working", x.getTime().equalsIgnoreCase(search));
+		}
+	}
+	@Test
+	public void testSearchFlightDestination() throws IOException {
+		setupScenary2();
+		setupScenary3();
+		String search = "Aruba";
+		Flight x = ap.searchFlightTime(search);
+		if(x != null) {
+		assertTrue("Not working", x.getTime().equalsIgnoreCase(search));
+		}
+	}
+	@Test
+	public void testSearchFlightGate() throws IOException {
+		setupScenary2();
+		setupScenary3();
+		int gate = 1;
+		Flight x = ap.searchFlightGate(gate);
+		if(x != null) {
+		assertTrue("Not working", x.getGate() == gate);
+		}
 	}
 
 }
